cee91c3544dd03c42bb182c0041a78ff59ff4bc0
Samantha-a-T <samantha.thibault@live.ca>
2022-10-13 13:31:07 -0600

    add icon and alt text from api

--- src/index.js
+++ src/index.js
@@ -1,20 +1,27 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
 
-function formatDate (timestamp){
-let date = new Date(timestamp);
-let hours= date.getHours();
-let minutes= date.getMinutes();
-if (minutes <10){
-    minutes= `0${minutes}`
-}
-if (hours < 10) {
-  hours = `0${hours}`;
-}
-let days= ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-let day= days[date.getDay()];
-return `${day} ${hours}:${minutes}`
-}
-
-function displayTemperature(response){
+function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   let cityElement = document.querySelector("#city");
@@ -24,13 +31,19 @@ function displayTemperature(response){
   let humiidityElement = document.querySelector("#humidity");
   humiidityElement.innerHTML = response.data.main.humidity;
   let windElement = document.querySelector("#wind");
-   windElement.innerHTML = Math.round(response.data.wind.speed);
-   let dateElement = document.querySelector("#date");
-    dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let city = "Paris";
 let apiKey = "f09d3949047ab6c9e3bcaf79cf61f619";
-let apiUrl= `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-axios.get(apiUrl).then(displayTemperature)
+axios.get(apiUrl).then(displayTemperature);
