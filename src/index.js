7dc0349b035d4eeee10d131ce091be4e15d0d1e3
Samantha-a-T <samantha.thibault@live.ca>
2022-10-13 10:59:38 -0600

    updated JS to include timestamp

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
+
+function formatDate (timestamp){
+let date = new Date(timestamp);
+let hours= date.getHours();
+let minutes= date.getMinutes();
+if (minutes <10){
+    minutes= `0${minutes}`
+}
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let days= ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+let day= days[date.getDay()];
+return `${day} ${hours}:${minutes}`
+}
+
 function displayTemperature(response){
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
@@ -9,9 +25,12 @@ function displayTemperature(response){
   humiidityElement.innerHTML = response.data.main.humidity;
   let windElement = document.querySelector("#wind");
    windElement.innerHTML = Math.round(response.data.wind.speed);
+   let dateElement = document.querySelector("#date");
+    dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 
 let apiKey = "f09d3949047ab6c9e3bcaf79cf61f619";
 let apiUrl= `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`
+
 axios.get(apiUrl).then(displayTemperature)
